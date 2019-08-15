d51c03b015b1e0e63badac282c8097f94dff2578
IzaacAyelin <47495047+IzaacAyelin@users.noreply.github.com>
2019-08-15 21:30:28 +0300

    Home loader

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -6,12 +6,14 @@ import Carousel from '../carousel/carousel';
 import { Link } from "react-router-dom";
 import { query } from '../../api/apiActions';
 import noImage from '../../images/no-image.jpg';
+import {MiniLoader} from '../search/mini-loader'
 
 
 class Home extends Component {
     constructor(props) {
         super(props);
         this._isMounted = false;
+        this.loading = true;
         this.state = {
             popularGames: [],
             recentGames: []
@@ -24,22 +26,26 @@ class Home extends Component {
             query: `where platforms = [6,48,49]`,
             offset: 0,
             sort: 'sort popularity desc',
-            limit:'4'
+            limit:'8'
         });
         let promis2 = query({
             query: `where platforms = [6,48,49]`,
             offset: 0,
             sort: 'sort first_release_dates desc',
-            limit:'4'
+            limit:'8'
         });
         
         Promise.all([promis1,promis2])
         .then((result)=>{
-            console.log(result);
-            this.setState({
-                popularGames:result[0].data,
-                recentGames:result[1].data
-            })
+           
+            this.loading=false;
+            if (this._isMounted) {
+                this.setState({
+                    popularGames:result[0].data,
+                    recentGames:result[1].data
+                }) 
+            }
+            
             
         })
     
@@ -65,45 +71,45 @@ class Home extends Component {
                 <div className="recent-games">
                     <h2 className="">Popular Games</h2>
                     <div className="row">
-                        {this.state.popularGames.map((game) => {
-                            console.log(game);
-
-                            let cover;
-                            if (game.cover && game.cover.url) {
-                                cover = `https://images.igdb.com/igdb/image/upload/t_cover_big/${game.cover.image_id}.jpg`;
-                            }
-                            else {
-                                cover = noImage;
-                            }
-                            return (<div key={game.id} className="col-md-3">
-                                <Link to={`/game/${game.id}`}>
-                                    <GameCard game={game} img={cover} />
-                                </Link>
-                            </div>)
-                        })}
-
+                    {this.loading ? <MiniLoader/> :
+                    (this.state.popularGames.map((game) => {
+                        let cover;
+                        if (game.cover && game.cover.url) {
+                            cover = `https://images.igdb.com/igdb/image/upload/t_cover_big/${game.cover.image_id}.jpg`;
+                        }
+                        else {
+                            cover = noImage;
+                        }
+                        return (<div key={game.id} className="col-md-3">
+                            <Link to={`/game/${game.id}`}>
+                                <GameCard game={game} img={cover} />
+                            </Link>
+                        </div>)
+                    }))
+                    }
+                        
                     </div>
                 </div>
                 <div className="recent-games">
                     <div>
                         <h2 className="">Recent Games</h2>
                         <div className="row">
-                            {this.state.recentGames.map((game) => {
-                                console.log(game);
-
-                                let cover;
-                                if (game.cover && game.cover.url) {
-                                    cover = `https://images.igdb.com/igdb/image/upload/t_cover_big/${game.cover.image_id}.jpg`;
-                                }
-                                else {
-                                    cover = noImage;
-                                }
-                                return (<div key={game.id} className="col-md-3">
-                                    <Link to={`/game/${game.id}`}>
-                                        <GameCard game={game} img={cover} />
-                                    </Link>
-                                </div>)
-                            })}
+                        {this.loading ? <MiniLoader/> :
+                    (this.state.recentGames.map((game) => {
+                        let cover;
+                        if (game.cover && game.cover.url) {
+                            cover = `https://images.igdb.com/igdb/image/upload/t_cover_big/${game.cover.image_id}.jpg`;
+                        }
+                        else {
+                            cover = noImage;
+                        }
+                        return (<div key={game.id} className="col-md-3">
+                            <Link to={`/game/${game.id}`}>
+                                <GameCard game={game} img={cover} />
+                            </Link>
+                        </div>)
+                    }))
+                    }
                         </div>
                     </div>
                 </div>
